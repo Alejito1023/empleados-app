8f779cac4c9e84a72c5565351c6e1067d89d39ea
Alejandro Parra <jparra102371@gmail.com>
2024-04-25 23:25:10 -0500

    Lista Empleado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\EmpleadoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empleados', [EmpleadoController::class,'index']);
